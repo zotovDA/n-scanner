c4a3303f85abe9413b82cf9dbef9d19dfb241689
ghspbravo <gmspbravo@gmail.com>
2020-07-25 17:47:15 +0500

    add bootstrap dependency

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,8 @@
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/tooltip';
+import 'bootstrap/js/dist/dropdown';
+
 import '../stylesheets/style.scss';
 
 function onInit() {
